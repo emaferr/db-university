6f6078b0d13d330aa9ab62b041243374337caf7d
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-07-05 20:57:43 +0200

    End JOINS queries

--- university2.sql
+++ university2.sql
@@ -2,25 +2,25 @@ UNIVERSITY DB
 
 GROUP_BY:
 
-- Contare quanti iscritti ci sono stati ogni anno
+1 Contare quanti iscritti ci sono stati ogni anno
 
 SELECT COUNT(id) AS numero_studenti, YEAR(enrolment_date) AS anno_iscrizione 
 FROM students 
 GROUP BY anno_iscrizione
 
-- Contare gli insegnanti che hanno ufficio nello stesso edificio
+2 Contare gli insegnanti che hanno ufficio nello stesso edificio
 
 SELECT COUNT(id) AS numero_insegnanti, office_address AS indirizzo_ufficio 
 FROM teachers 
 GROUP BY indirizzo_ufficio
 
-- Calcolare la media dei voti di ogni appello di esame
+3 Calcolare la media dei voti di ogni appello di esame
 
 SELECT exam_id AS id_esame , ROUND(AVG(vote),2) AS media_voti 
 FROM exam_student 
 GROUP BY id_esame
 
-- Contare quanti corsi di laurea ci sono per ogni dipartimento
+4 Contare quanti corsi di laurea ci sono per ogni dipartimento
 
 SELECT degrees.name AS nome_laurea, courses.degree_id AS id_laurea, COUNT(courses.id) AS numero_corsi 
 FROM degrees 
@@ -29,25 +29,58 @@ GROUP BY id_laurea
 
 JOINS:
 
-- Selezionare tutti gli studenti iscritti al Corso di Laurea Economia
+1 Selezionare tutti gli studenti iscritti al Corso di Laurea Economia
 
 SELECT students.* 
 FROM degrees 
 JOIN students ON degrees.id = students.degree_id 
 WHERE degrees.name = "Corso di Laurea in Economia"
 
-- Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+2 Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 
 SELECT degrees.* 
 FROM departments 
 JOIN degrees ON departments.id = degrees.department_id 
 WHERE departments.name = "Dipartimento di Neuroscienze"
 
-- Selezionare tutti i corsi dove insegna Fulvio Amato (id=44)
+3 Selezionare tutti i corsi dove insegna Fulvio Amato (id=44)
 
 SELECT `courses`.`name` 
 FROM courses 
 JOIN course_teacher ON courses.id = course_teacher.course_id 
 WHERE course_teacher.teacher_id = 44
 
+4 Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, ordine alfabetico per cognome e nome 
 
+SELECT `students`.`surname` AS cognome_studente, `students`.`name` AS nome_studente, `degrees`.`name` AS nome_laurea, `degrees`.`level` AS livello_laurea, `departments`.`name` AS nome_dipartimento 
+FROM departments 
+JOIN degrees ON departments.id = degrees.department_id 
+JOIN students ON degrees.id = students.degree_id 
+ORDER BY cognome_studente ASC, nome_studente ASC
+
+5 Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+SELECT `degrees`.`name` AS nome_laurea, `courses`.`name` AS nome_corso, `teachers`.`name` AS nome_insegnante, `teachers`.`surname` AS cognome_insegnante 
+FROM degrees 
+JOIN courses ON degrees.id = courses.degree_id 
+JOIN course_teacher ON course_teacher.course_id = courses.id 
+JOIN teachers ON course_teacher.teacher_id = teachers.id
+
+6 Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+
+SELECT COUNT(teachers.id)
+FROM departments
+JOIN degrees ON departments.id = degrees.department_id
+JOIN courses ON degrees.id = courses.degree_id
+JOIN course_teacher ON course_teacher.course_id = courses.id 
+JOIN teachers ON course_teacher.teacher_id = teachers.id
+WHERE `departments`.`id` = 5
+// Il risultato e 70 non riesco ad ottenere 54!
+
+7 BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+
+SELECT students.name, students.surname, COUNT(exam_student.exam_id GROUP BY exam_student.exam_id ) AS tentativi
+FROM students
+JOIN exam_student ON students.id = exam_student.exam_id
+GROUP BY students.id
+// Ancora incompleto
